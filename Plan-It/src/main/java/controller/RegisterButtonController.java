dac7384fa151ed9f56a619d05e338a21977965a4
samuele <samuelerovito002@gmail.com>
2024-11-08 12:10:08 +0100

    controllare la correttezza dell'email durante la registrazione

--- Plan-It/src/main/java/controller/RegisterButtonController.java
+++ Plan-It/src/main/java/controller/RegisterButtonController.java
@@ -1,14 +1,28 @@
 package controller;
 
+import model.EmailValidator;
 import view.ApplicationWindow;
 import view.panel.DeskView;
+import view.panel.RegisterView;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class RegisterButtonController implements ActionListener {
+public class RegisterButtonController implements ActionListener
+{
     @Override
-    public void actionPerformed(ActionEvent e) {
+    public void actionPerformed(ActionEvent e)
+    {
+        RegisterView registerView = (RegisterView) ((JButton) e.getSource()).getParent();
+
+        String email = registerView.getEmail();
+        if (!EmailValidator.isValid(email))
+        {
+            JOptionPane.showMessageDialog(registerView, "Invalid email format", "Error", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+
         ApplicationWindow.getInstance().setPanel(new DeskView());
     }
 }
