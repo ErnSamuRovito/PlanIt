94f31cb3f2c367da0fa0bcc5649d7933f1ad44c2
samuele <samuelerovito002@gmail.com>
2024-11-11 17:02:53 +0100

    implementata classe per l'invio delle notifiche (ubuntu)

--- Plan-It/src/main/java/model/DesktopNotification.java
+++ Plan-It/src/main/java/model/DesktopNotification.java
@@ -6,7 +6,6 @@ public class DesktopNotification {
 
     public static void sendNotification(String title, String message) {
         try {
-            // Esegui il comando notify-send per inviare la notifica
             String[] command = { "notify-send", title, message };
             ProcessBuilder processBuilder = new ProcessBuilder(command);
             processBuilder.start();
